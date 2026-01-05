11c3fd42ddbf36e33f5770d0a705e24386ab84b0
zakiquereshy-arvaya <zakiquereshy@arvayaconsulting.com>
2026-01-04 19:24:15 -0500

    Update ai_calendar_helpers.py

--- ai_calendar_helpers.py
+++ ai_calendar_helpers.py
@@ -20,7 +20,7 @@ class CalendarAIHelper:
     
     def __init__(self):
         api_key = os.getenv('AZURE_OPENAI_API_KEY')
-        api_version = os.getenv('AZURE_OPENAI_API_VERSION', '2024-02-15-preview')
+        api_version = os.getenv('AZURE_OPENAI_API_VERSION', '2024-12-01-preview')
         azure_endpoint = os.getenv('AZURE_OPENAI_ENDPOINT')
         model = os.getenv('AZURE_OPENAI_MODEL')
         
